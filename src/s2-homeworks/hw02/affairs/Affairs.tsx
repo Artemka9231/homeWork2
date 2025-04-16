98f0de4a5b6befb46005d432eb138f482ab6e2c3
SU-30SM <Artemka9231@gmail.com>
2025-04-16 15:05:42 +0300

    homeWork.2.1

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -1,33 +1,21 @@
 import React from 'react'
-import Affair from './affair/Affair'
-import {AffairType, FilterType} from '../HW2'
+
+import { AffairType, FilterType } from '../HW2'
 import s from './Affairs.module.css'
+import Affair from "./affair/Affair";
 
 type AffairsPropsType = {
-    data: AffairType[] // need to fix any
+    data: AffairType[]
     setFilter: (filter: FilterType) => void
     deleteAffairCallback: (_id: number) => void
     filter: FilterType
 }
 
-
 function Affairs(props: AffairsPropsType) {
-    const setAll = () => {props.setFilter('all')
-        // need to fix
-    }
-    const setHigh = () => {
-        props.setFilter('high')
-        // need to fix
-    }
-    const setMiddle = () => {
-        props.setFilter('middle')
-        // need to fix
-    }
-    const setLow = () => {
-        props.setFilter('low')
-        // need to fix
-    }
-
+    const setAll = () => props.setFilter('all')
+    const setHigh = () => props.setFilter('high')
+    const setMiddle = () => props.setFilter('middle')
+    const setLow = () => props.setFilter('low')
 
     const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
     const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ? ' ' + s.active : '')
@@ -36,7 +24,7 @@ function Affairs(props: AffairsPropsType) {
 
     const mappedAffairs = props.data.map((a: AffairType) => (
         <Affair
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+            key={a._id}
             affair={a}
             deleteAffairCallback={props.deleteAffairCallback}
         />
