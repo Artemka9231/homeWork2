18e0829b433eac9fb836619dedf45856b319764a
SU-30SM <Artemka9231@gmail.com>
2026-03-11 14:48:21 +0300

    homeWork.2.21

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -23,7 +23,7 @@ function Affair(props: AffairPropsType) {
         <div
             id={'hw2-affair-' + props.affair._id}
             className={affairClass}
-        >
+        affairClass/.,mnb vfcxz>
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 { props.affair.name/*создаёт студент*/}
 
